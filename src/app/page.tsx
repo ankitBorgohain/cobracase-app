de104283bca0c37b2f5e1fba039d7ff8702343ee
ankitBorgohain <ankit.borgohain98@gmail.com>
2024-06-11 01:49:45 +0530

    Home pt-1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,50 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { Check } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="bg-gray-50">
+    <div className="bg-gray-200 ">
       <section>
-        <MaxWidthWrapper className="pt-10 pb-20 lg:grid lg:grid-cols-3 sm:pb-32 
-        lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52 border border-red-900">
-          <div className="col-span-2 px-6 lg:px-0 lg:pt-4 border border-black">
-            hi
-            <div className="relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start border border-green-900">
+        <MaxWidthWrapper
+          className="pt-10 pb-20 lg:grid lg:grid-cols-3 sm:pb-32 
+        lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52 "
+        >
+          <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
+            <div className="relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start">
               <div className="absolute w-28 left-0 -top-20 hidden lg:block">
-                <img src="/snake1.png"/>
+                <img src="/snake-1.png" className="w-full" />
               </div>
+              <h1 className="relative w-fit tracking-tight text-balance font-bold mt-16 !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl">
+                Your Image on a{" "}
+                <span className="bg-green-500 px-2 text-white">Custom</span>{" "}
+                Phone Case
+              </h1>
+              <p className="mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap">
+                Capture your favourite memories with your own,{" "}
+                <span className="font-semibold text-green-800 bg-white px-1">one-of-one</span> phone case.
+                CaseCobra allows you to protect your memories, not just your
+                phone case.
+              </p>
+
+              <ul className="mt-8 space-y-2 text-left font-medium flex flex-col items-center sm:items-start">
+                <div className="space-y-2">
+                  <li className="flex gap-1.5 items-center text-left">
+                   <Check className="h5 w-5 shrink-0 text-green-600"/>
+                    High-quality, durable material
+                  </li>
+                  <li className="flex gap-1.5 items-center text-left">
+                   <Check className="h5 w-5 shrink-0 text-green-600"/>
+                    5 year print garuntee
+                  </li>
+                  <li className="flex gap-1.5 items-center text-left">
+                   <Check className="h5 w-5 shrink-0 text-green-600 "/>
+                    Modern iPhone models supported
+                  </li>
+                </div>
+              </ul>
+              <div className="mt-12 flex flex-col sm"></div>
+
             </div>
           </div>
         </MaxWidthWrapper>
