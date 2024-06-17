007dd3725e8d1ddcb57ec4589d429c54d1a1bca8
ankitBorgohain <ankit.borgohain98@gmail.com>
2024-06-17 19:38:31 +0530

    More work on the landing page| initialized kinde Auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
-import { Check } from "lucide-react";
-import Image from "next/image";
+import Phone from "@/components/Phone";
+import { Check, Star } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="bg-gray-200 ">
+    <div className= "bg-slate-50">
       <section>
         <MaxWidthWrapper
           className="pt-10 pb-20 lg:grid lg:grid-cols-3 sm:pb-32 
@@ -22,29 +22,88 @@ export default function Home() {
               </h1>
               <p className="mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap">
                 Capture your favourite memories with your own,{" "}
-                <span className="font-semibold text-green-800 bg-white px-1">one-of-one</span> phone case.
-                CaseCobra allows you to protect your memories, not just your
-                phone case.
+                <span className="font-semibold text-green-800 bg-white px-1">
+                  one-of-one
+                </span>{" "}
+                phone case. CaseCobra allows you to protect your memories, not
+                just your phone case.
               </p>
 
               <ul className="mt-8 space-y-2 text-left font-medium flex flex-col items-center sm:items-start">
                 <div className="space-y-2">
                   <li className="flex gap-1.5 items-center text-left">
-                   <Check className="h5 w-5 shrink-0 text-green-600"/>
+                    <Check className="h5 w-5 shrink-0 text-green-600" />
                     High-quality, durable material
                   </li>
                   <li className="flex gap-1.5 items-center text-left">
-                   <Check className="h5 w-5 shrink-0 text-green-600"/>
-                    5 year print garuntee
+                    <Check className="h5 w-5 shrink-0 text-green-600" />5 year
+                    print garuntee
                   </li>
                   <li className="flex gap-1.5 items-center text-left">
-                   <Check className="h5 w-5 shrink-0 text-green-600 "/>
+                    <Check className="h5 w-5 shrink-0 text-green-600 " />
                     Modern iPhone models supported
                   </li>
                 </div>
               </ul>
-              <div className="mt-12 flex flex-col sm"></div>
+              <div
+                className="mt-12 flex flex-col sm:flex-row items-center
+            sm:items-start gap-5"
+              >
+                <div className="flex -space-x-4">
+                  <img
+                    className="inline-block h-10 w-10 ring-2 ring-gray-100 rounded-full"
+                    src="/users/user-1.png"
+                    alt="user image"
+                  />
+                  <img
+                    className="inline-block h-10 w-10 ring-2 ring-gray-100 rounded-full"
+                    src="/users/user-2.png"
+                    alt="user image"
+                  />
+                  <img
+                    className="inline-block h-10 w-10 ring-2 ring-gray-100 rounded-full"
+                    src="/users/user-3.png"
+                    alt="user image"
+                  />
+                  <img
+                    className="inline-block h-10 w-10 ring-2 ring-gray-100 rounded-full"
+                    src="/users/user-4.jpg"
+                    alt="user image"
+                  />
+                  <img
+                    className="inline-block object-cover h-10 w-10 ring-2 ring-gray-100 rounded-full"
+                    src="/users/user-5.jpg"
+                    alt="user image"
+                  />
+                </div>
 
+                <div className="flex flex-col justify-between items-center sm-items-start">
+                  <div className="flex gap-0.5">
+                    <Star className="h-5 w-5 text-yellow-500 fill-yellow-400 " />
+                    <Star className="h-5 w-5 text-yellow-500 fill-yellow-400 " />
+                    <Star className="h-5 w-5 text-yellow-500 fill-yellow-400 " />
+                    <Star className="h-5 w-5 text-yellow-500 fill-yellow-400 " />
+                    <Star className="h-5 w-5 text-yellow-500 fill-yellow-400 " />
+                  </div>
+                  <p>
+                    <span className="font-semibold">1.250</span> happy customers
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-span-full lg:col-span-1 w-full flex justify-center px-8 sm:px-16 md:px-0 mt-32 lg:mx-0 lg:mt-20 h-fit">
+            <div className="relative md:max-w-xl border border-black-100">
+              <img
+                className="absolute w-40 lg:w-52 left-56 -top-20 select-none hidden sm:block lg:hidden xl:block"
+                src="/your-image.png"
+              />
+              <img
+                src="/line.png"
+                className="absolute w-20 -left-6 -bottom-6 select-none"
+              />
+              <Phone imgSrc="/testimonials/1.jpg" className="w-64"/>
             </div>
           </div>
         </MaxWidthWrapper>
